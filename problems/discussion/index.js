46d70754e5c4a9f30f9f28b51fe66072b0bf3424
Monico Moreno <cdnadmin@collaborare.net>
2016-12-06 13:51:49 -0600

    added verification for discussion problem

--- problems/discussion/index.js
+++ problems/discussion/index.js
@@ -1,10 +1,40 @@
 var fs = require('fs');
 var path = require('path');
+var exec = require('child_process').exec
 
 exports.problem = fs.createReadStream(__dirname + '/problem.txt');
 exports.solution = fs.createReadStream(__dirname + '/solution.txt');
 
 exports.verify = function (args, cb) {
-    cb(false);
-    console.log("Actual solution verification coming soon!\n");
+	/*
+		Note:
+		The bulk of this solution was borrowed from https://github.com/jlord/git-it/blob/master/problems/repository/verify.js
+		Credit goes @jlord
+	*/
+
+	exec('git status', function(err, stdout, stdrr) {
+	  var status = stdout.trim()
+	  if (status.match("On branch")){
+		console.log("This is a Git repository!\n");
+
+		//todo: careful w/callback hell here.
+		exec('git reflog show origin/master', function(err, stdout, stderr) {
+		  var ref = stdout.trim()
+
+		  if (ref.match("update by push")){
+			console.log("...and detected a push!  Nicely done!");
+			cb(true);
+		  }
+		  else {
+			console.log("...but no evidence of a push.");
+			cb(false);
+		}})
+
+	  }
+	  else{
+		cb(false);
+		console.log("This folder isn't being tracked by Git.\n");
+	  }
+	});
+
 };
